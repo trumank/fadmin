0a45e09f54feb5f321e9b3145c0f04b966402846
Truman Kilen <tkilen@mm.st>
2019-04-24 12:50:40 -0500

    Use factorio-rcon-py

--- fadmin/__init__.py
+++ fadmin/__init__.py
@@ -4,82 +4,61 @@ import asyncio
 import discord
 import json
 import os
-import socket
 import sys
 import threading
+import factorio_rcon
 from time import sleep
-from construct import *
 from dotenv import load_dotenv
 
 load_dotenv()
 
-packet = Prefixed(Int32sl, Struct(
-    'id' / Int32sl,
-    'type' / Int32sl,
-    'body' / CString("utf8"),
-    Default(CString("utf8"), '')
-))
-
 CHANNEL = int(os.getenv('DISCORD_CHANNEL'))
 
 def run():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((os.getenv('RCON_HOST'), int(os.getenv('RCON_PORT'))))
-        file = sock.makefile('rwb')
+    rcon = factorio_rcon.RCONClient(os.getenv('RCON_HOST'), int(os.getenv('RCON_PORT')), os.getenv('RCON_PWD'))
 
-        file.write(packet.build({'id': 0, 'type': 3, 'body': os.getenv('RCON_PWD')}))
-        file.flush()
+    async def poll():
+        while True:
+            for msg in json.loads(rcon.send_command('/sc remote.call("fadmin", "poll")')):
+                yield msg
+            await asyncio.sleep(.1)
 
-        def get():
-            file.write(packet.build({'id': 2, 'type': 2, 'body': '/sc remote.call("fadmin", "poll")'}))
-            file.flush()
-            return packet.parse_stream(file)
+    client = discord.Client()
 
-        async def poll():
-            while True:
-                p = await client.loop.run_in_executor(None, get())
-                if p.type == 0 and p.id == 2:
-                    for msg in json.loads(p.body):
-                        yield msg
-                await asyncio.sleep(.1)
+    async def my_background_task():
+        await client.wait_until_ready()
+        counter = 0
+        channel = client.get_channel(CHANNEL)
+        async for msg in poll():
+            if not client.is_closed:
+                break;
+            if msg['type'] == 'chat':
+                str = msg['name'] + ': ' + msg['message']
+                asyncio.ensure_future(channel.send(str))
+            if msg['type'] == 'left':
+                str = '*' + msg['name'] + ' left*'
+                asyncio.ensure_future(channel.send(str))
+            if msg['type'] == 'joined':
+                str = '*' + msg['name'] + ' joined*'
+                asyncio.ensure_future(channel.send(str))
 
-        client = discord.Client()
+    @client.event
+    async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
 
-        async def my_background_task():
-            await client.wait_until_ready()
-            counter = 0
-            channel = client.get_channel(CHANNEL)
-            async for msg in poll():
-                if not client.is_closed:
-                    break;
-                if msg['type'] == 'chat':
-                    str = msg['name'] + ': ' + msg['message']
-                    asyncio.ensure_future(channel.send(str))
-                if msg['type'] == 'left':
-                    str = '*' + msg['name'] + ' left*'
-                    asyncio.ensure_future(channel.send(str))
-                if msg['type'] == 'joined':
-                    str = '*' + msg['name'] + ' joined*'
-                    asyncio.ensure_future(channel.send(str))
+    @client.event
+    async def on_message(message):
+        if message.author == client.user or message.channel.id != CHANNEL:
+            return
+        name = message.author.nick or message.author.name
+        escaped = '"' + (name + '*: ' + message.content).replace('\\', '\\\\').replace('"', '\\"') + '"';
+        rcon.send_command('/sc game.print(' + escaped + ', {.7,.7,.7})')
 
-        @client.event
-        async def on_ready():
-            print('Logged in as')
-            print(client.user.name)
-            print(client.user.id)
-            print('------')
-
-        @client.event
-        async def on_message(message):
-            if message.author == client.user or message.channel.id != CHANNEL:
-                return
-            name = message.author.nick or message.author.name
-            str = '"' + (name + '*: ' + message.content).replace('\\', '\\\\').replace('"', '\\"') + '"';
-            file.write(packet.build({'id': 3, 'type': 2, 'body': '/sc game.print(' + str + ', {.7,.7,.7})'}))
-            file.flush()
-
-        client.loop.create_task(my_background_task())
-        client.run(os.getenv('DISCORD_TOKEN'))
+    client.loop.create_task(my_background_task())
+    client.run(os.getenv('DISCORD_TOKEN'))
 
 run()
 
