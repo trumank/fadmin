5f5331e0974fcc305812b99c99ca335e31883295
Truman Kilen <trumank@users.noreply.github.com>
2020-08-31 00:38:44 -0500

    Fix syntax error and change catch-all death message

--- fadmin/__init__.py
+++ fadmin/__init__.py
@@ -98,7 +98,7 @@ def main():
                 elif msg['cause']['type'] == 'artillery-turret':
                     cause = ' was mistaken for the enemy'
                 else:
-                    cause = f' died of mysterious causes. {msg['cause']['type']}'
+                    cause = f' was killed by {msg["cause"]["type"]}'
 
                 str = '*' + msg['name'] + cause + '*'
                 asyncio.ensure_future(channel.send(discord.utils.escape_mentions(str)))
